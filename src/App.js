50c442d0534d4f82bd100cf414528a3f82e75c72
Tanya Swift <tanyanicolerussi@gmail.com>
2018-11-08 14:19:10 -0700

    Checkpoint React To-Do Events: completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
-import ToDo from './components/ToDo.js';
+import ToDo from './components/ToDo.js'; //Here we are importing information from our ToDo.js file
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ul>
-          <ToDo />
-          <ToDo />
-        </ul>
-      </div>
-    );
-  }
-}
+   constructor(props) { //We need the constructor function when creating a new Class, and super when extending a class
+     super(props);
+      this.state = { //Here we are creating a state object (todos) which can be accessed anywhere from the class
+        todos: [ //todos array w/key/value pairs
+          { description: 'Walk the cat', isCompleted: true },
+          { description: 'Throw the dishes away', isCompleted: false },
+          { description: 'Buy new dishes', isCompleted: false }
+        ],
+        newTodoDescription: '' //this will be for what the user wants to call their list item
+      };
+   }
+
+   handleChange(e) {
+     this.setState({ newToDoDescription: e.target.value })
+   }
+
+   handleSubmit(e){
+     e.preventDefault();//prevents the default page from reloading on user submit
+      if (!this.state.newTodoDescription) { return }
+     const newTodo = { description: this.state.newToDoDescription, isCompleted: false };
+      this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' }); //Here we are creating a new item in our todo array
+   } //...is JavaScript spread syntax to add an object to the end of a new array
+
+   toggleComplete(index) { //event handler, index is selecting the index number of the array
+     const todos = this.state.todos.slice(); //slice() is making a copy of the array
+     const todo = todos[index];
+     todo.isCompleted = todo.isCompleted ? false : true;
+     this.setState({ todos: todos});
+   }
+
+   render() { //this is where we are actually rending the data to the UI
+     return (
+       <div className="App">
+         <ul>
+           { this.state.todos.map( (todo, index) => //We are using .map() to iterate over the todos array and return a new array
+             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+             //we are using this line to pass the data down to our ToDo component-toggleComplete function
+           )}
+         </ul>
+         <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={ this.state.newToDoDescription } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" />
+         </form>
+       </div>
+     );//e is short for event, and it contains event data passed to the function by the browser
+     //props look like HTML attributes w/ a key and a value separated by an equal sign
+   }
+ }
 
 export default App;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render () {
     return (
-      <li> A todo will go here </li>
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+        <span>{ this.props.description }</span>
+      </li>
     );
   }
 }
-
-export default ToDo;
+ // <span>{ this.props.description }</span> Here we are accessing props
+//onChange is an event listener that will access the toggleComplete event handler
+export default ToDo; //this allows us to export to another component
